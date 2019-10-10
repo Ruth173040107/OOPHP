f2c5e1af8340dbaac8bd6d8533d3218bf3e0cd08
ruthsaanah <173040107@mail.unpas.ac.id>
2019-10-10 15:13:41 +0700

    menambahkan konsep overriding

--- inheritance-problem.php
+++ inheritance-problem.php
@@ -6,20 +6,16 @@ class Produk {
 	public $judul,
 		   $penulis,
 		   $penerbit,
-		   $harga,
-		   $jmlHalaman,
-		   $waktuMain;
+		   $harga;
 	
 
 
-	public function __construct($judul = 'judul', $penulis  = 'penulis', $penerbit = 'penerbit' , $harga = 0, $jmlHalaman = 0, $waktuMain = 0)
+	public function __construct($judul = 'judul', $penulis  = 'penulis', $penerbit = 'penerbit' , $harga = 0)
 	 {
 	 	$this->judul = $judul;
 	 	$this->penulis = $penulis;
 	 	$this->penerbit = $penerbit;
 	 	$this->harga = $harga;
-	 	$this->jmlHalaman = $jmlHalaman;
-	 	$this->waktuMain = $waktuMain;
 	 
 	 } 
 
@@ -38,18 +34,34 @@ class Produk {
 
 class Komik extends Produk
 {
+	public $jmlHalaman;
+
+	public function __construct($judul = 'judul', $penulis  = 'penulis', $penerbit = 'penerbit' , $harga = 0, $jmlHalaman = 0)
+	 {
+	 	parent::__construct($judul, $penulis, $penerbit , $harga);
+	 	$this->jmlHalaman = $jmlHalaman;
+	 
+	 } 
 	public function getInfoProduk()
 	{
-		$str = "Komik: {$this->judul} | {$this->getLabel()} (Rp. {$this->harga}) - {$this->jmlHalaman} Halaman.";
+		$str = "Komik: " . parent::getInfoProduk() . " {$this->jmlHalaman} Halaman.";
 		return $str;
 	}
 }
 
 class Game extends Produk
 {
+	public $waktuMain;
+	
+	public function __construct($judul = 'judul', $penulis  = 'penulis', $penerbit = 'penerbit' , $harga = 0, $waktuMain = 0)
+	 {
+	 	parent::__construct($judul, $penulis, $penerbit, $harga);
+	 	$this->waktuMain = $waktuMain;
+	 
+	 } 
 	public function getInfoProduk()
 	{
-		$str = "Game: {$this->judul} | {$this->getLabel()} (Rp. {$this->harga}) - {$this->waktuMain} Jam.";
+		$str = "Game: " . parent::getInfoProduk() . " {$this->waktuMain} Jam.";
 		return $str;
 	}
 }
@@ -64,8 +76,8 @@ class CetakInfoProduk
 }
 
 
-$produk1 = new Komik("naruto", "massashi kisihimoto","shonen jump", 30000, 100, 0);
-$produk2 = new Game("uncarted", "neil Druckmann", " Sony Computer", 250000, 0, 50);
+$produk1 = new Komik("naruto", "massashi kisihimoto","shonen jump", 30000, 100);
+$produk2 = new Game("uncarted", "neil Druckmann", " Sony Computer", 250000, 50);
 
 echo $produk1->getInfoProduk();
 echo "<br>";
